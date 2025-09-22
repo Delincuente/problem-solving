ccefdd3becfca52eda45248a648ad62f16bb8969
CRIMINAL <butani.hardik53@gmail.com>
2025-09-22 10:53:58 +0530

    matrix reshape

---
+++ leetCode/matrixReshape.js
@@ -0,0 +1,23 @@
+// You are given an m x n matrix mat and two integers r and c representing 
+// the number of rows and the number of columns of the wanted reshaped matrix.
+
+// The reshaped matrix should be filled with all the elements of the original 
+// matrix in the same row-traversing order as they were.
+
+// If the reshape operation with given parameters is possible and legal, 
+// output the new reshaped matrix; Otherwise, output the original matrix.
+
+function matrixReshape(mat, r, c) {
+    const row = mat.length, col = mat[0].length;
+    if (row * col == r * c) {
+        const reshape = [];
+        let 
+        for (let i = 0; i < mat.length; i++) {
+            for (let j = 0; j < mat.length; j++) {
+
+            }
+        }
+    } else {
+        return mat;
+    }
+}
